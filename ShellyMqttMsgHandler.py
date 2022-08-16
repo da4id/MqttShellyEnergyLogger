2c57568e56fffd9dc907981e85a864e1cb9bee5a
David Zingg <mail@davidzingg.ch>
2022-08-16 22:10:16 +0200

    - Shelly Plus 1PM integrieren

--- ShellyMqttMsgHandler.py
+++ ShellyMqttMsgHandler.py
@@ -46,6 +46,7 @@ class ShellyMqttMsgHandler(mqtt.Client):
                     device = session.query(Device).filter_by(id=deviceId).first()
                     if device is None:
                         self.logger.warning("nicht registriertes Device: " + deviceId)
+                        session.close()
                         return
                     idx = msg.topic.find("/relay/") + len("/relay/")
                     channelId = int(msg.topic[idx:idx + 1])
@@ -67,9 +68,13 @@ class ShellyMqttMsgHandler(mqtt.Client):
                     self.logger.debug("Channel " + str(channel.channelId))
 
                     if msg.topic.find("energy") > 0:
-                        self._process_energy(payload, device, series, channel, session)
+                        energy = round(int(payload) / (60.0 * 1000), 3)
+                        self._process_energy(energy, device, series, channel, session)
                     elif msg.topic.find("power") > 0:
-                        self._process_power(payload, device, series, channel, session)
+                        power = float(payload)
+                        self._process_power(power, series, channel, session)
+                else:
+                    self._process_plus(msg.topic, msg.payload, session)
                 session.close()
             except Exception as e:
                 session.rollback()
@@ -79,8 +84,7 @@ class ShellyMqttMsgHandler(mqtt.Client):
         except Exception as e:
             self.logger.warning(e)
 
-    def _process_energy(self, payload, device, series, channel, session):
-        energy = round(int(payload) / (60.0 * 1000), 3)
+    def _process_energy(self, energy, device, series, channel, session):
         if int(energy * 1000) < int(channel.energy * 1000):
             self.logger.info("Energy is Lower than db Record -> Create new Series")
             self.logger.info(str(energy) + " " + str(channel.energy))
@@ -92,18 +96,16 @@ class ShellyMqttMsgHandler(mqtt.Client):
             session.commit()
         else:
             self.logger.warning("channel is None (Energy) series: " + str(series.dbid))
-        self.logger.debug("process_energy " + payload)
-        pass
+        self.logger.debug("process_energy " + str(energy))
 
-    def _process_power(self, payload, device, series, channel, session):
+    def _process_power(self, power, series, channel, session):
         if channel is not None:
-            measurement = Measurement(float(payload), channel)
+            measurement = Measurement(power, channel)
             session.add(measurement)
             session.commit()
         else:
             self.logger.warning("channel is None (Energy) series: " + str(series.dbid))
-        self.logger.debug("process_power " + payload)
-        pass
+        self.logger.debug("process_power " + str(power))
 
     def _process_announce(self, payload, session):
         d = json.loads(payload)
@@ -118,6 +120,36 @@ class ShellyMqttMsgHandler(mqtt.Client):
 
         session.commit()
 
+    def _process_plus(self, topic, payload, session):
+        deviceId = topic[0:topic.find("/status/switch")]
+        device = session.query(Device).filter_by(id=deviceId).first()
+        if device is None:
+            self.logger.warning("nicht registriertes Device: " + deviceId)
+            return
+        idx = topic.find("switch:") + len("switch:")
+        channelId = int(topic[idx:idx + 1])
+        series = session.query(Series).filter_by(dbIdDevice=device.dbid).order_by(
+            Series.startTimestamp.desc()).first()
+        self.logger.debug("device " + str(device.dbid))
+        self.logger.debug("channelId " + str(channelId))
+        if series is None:
+            self._create_new_series(device, session)
+            session.commit()
+        self.logger.debug("series " + str(series.dbid))
+
+        channel = session.query(Channel).filter_by(series=series).filter_by(channelId=channelId).first()
+
+        if channel is None:
+            channel = Channel(series, channelId, 0)
+            session.add(channel)
+            session.commit()
+        self.logger.debug("Channel " + str(channel.channelId))
+
+        data = json.loads(payload.decode('utf-8'))
+
+        self._process_energy(data["aenergy"]["total"] / 1000, device, series, channel, session)
+        self._process_power(data["apower"], series, channel, session)
+
     def _create_new_series(self, device, session):
         self.logger.info("Create new series for Device " + device.id)
         series = Series(device)
@@ -130,6 +162,9 @@ class ShellyMqttMsgHandler(mqtt.Client):
             channel1 = Channel(series, 1, 0)
             session.add(channel0)
             session.add(channel1)
+        elif device.model == "shellyplus1pm":
+            channel = Channel(series, 0, 0)
+            session.add(channel)
         return series
 
     def _subscribe_device(self, device):
@@ -143,6 +178,9 @@ class ShellyMqttMsgHandler(mqtt.Client):
             self.subscribe("shellies/" + device.id + "/relay/1/power", 0)
             self.subscribe("shellies/" + device.id + "/relay/0/energy", 0)
             self.subscribe("shellies/" + device.id + "/relay/1/energy", 0)
+        elif device.model == "shellyplus1pm":
+            self.logger.info("Subscribe Shelly Plus 1PM: " + device.id)
+            self.subscribe(device.id + "/status/switch:0", 0)
 
     def run(self, username, password, server, port):
         self.logger.info("Connect to MqTT Broker")
